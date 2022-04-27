7de07e3294bd35159ac6f2ec86a8fd97489cbc9a
Kakalanp <jgarridoleccazanetti@gmail.com>
2022-04-27 12:09:58 +0000

    Adding transactions and new queries, plus a few changes in structure

--- data.sql
+++ data.sql
@@ -1,16 +1,16 @@
 /* Populate database with sample data. */
 
 INSERT INTO animals (id, name, date_of_birth, weight_kg, neutered, escape_attempts)
-VALUES (1, 'Agumon', 'Feb 3rd, 2020', 10.23, 1, 0),
-(2, 'Gabumon', 'Nov 15th, 2018', 8, 1, 2),
-(3, 'Pikachu', 'Jan 7th, 2021', 15.04, 0, 1),
-(4, 'Devimon', 'May 12th, 2017', 11, 1, 5);
+VALUES (1, 'Agumon', 'Feb 3rd, 2020', 10.23, true, 0),
+(2, 'Gabumon', 'Nov 15th, 2018', 8, true, 2),
+(3, 'Pikachu', 'Jan 7th, 2021', 15.04, false, 1),
+(4, 'Devimon', 'May 12th, 2017', 11, true, 5);
 
 INSERT INTO animals (id, name, date_of_birth, weight_kg, neutered, escape_attempts)
-VALUES (5, 'Charmander', 'Feb 8th, 2020', -11, 0, 0),
-(6, 'Plantmon', 'Nov 15th, 2021', -5.7, 1, 2),
-(7, 'Squirtle', 'Apr 2nd, 1993', -12.13, 0, 3),
-(8, 'Angemon', 'Jun 12th, 2005', -45, 1, 1),
-(9, 'Boarmon', 'Jun 7th, 2005', 20.4, 1, 7),
-(10, 'Blossom', 'Oct 13th, 1998', 17, 1, 3),
-(11, 'Ditto', 'May 14th, 2022', 22, 1, 4);
+VALUES (5, 'Charmander', 'Feb 8th, 2020', -11, false, 0),
+(6, 'Plantmon', 'Nov 15th, 2021', -5.7, true, 2),
+(7, 'Squirtle', 'Apr 2nd, 1993', -12.13, false, 3),
+(8, 'Angemon', 'Jun 12th, 2005', -45, true, 1),
+(9, 'Boarmon', 'Jun 7th, 2005', 20.4, true, 7),
+(10, 'Blossom', 'Oct 13th, 1998', 17, true, 3),
+(11, 'Ditto', 'May 14th, 2022', 22, true, 4);
--- schema.sql
+++ schema.sql
@@ -10,4 +10,4 @@ CREATE TABLE animals (
   PRIMARY KEY(id)
 );
 
-ALTER TABLE animals ADD COLUMN species VARCHAR;
+ALTER TABLE animals ADD COLUMN species VARCHAR(250);
