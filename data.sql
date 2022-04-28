85a25eaca7be6d1a0d524b45726e5690772cd919
Kakalanp <jgarridoleccazanetti@gmail.com>
2022-04-27 21:18:06 -0500

    adding data to new tables

--- data.sql
+++ data.sql
@@ -36,3 +36,37 @@ Set owner_id = 2 WHERE name IN ('Gabumon', 'Pikachu'),
 Set owner_id = 3 WHERE name IN ('Devimon', 'Plantmon'),
 Set owner_id = 4 WHERE name IN ('Charmander', 'Squirtle', 'Blossom'),
 Set owner_id = 5 WHERE name IN ('Angemon','Boarmon');
+
+INSERT INTO vets(name, age, date_of_graduation)
+VALUES ('William Tatcher', 45, 'Apr 23rd, 2000'),
+('Maisy Smith', 26, 'Jan 17th, 2019'),
+('Stephanie Mendez',64, 'May 4th, 1981'),
+('Jack Harkness', 38, 'Jun 8th, 2008');
+
+INSERT INTO specializations (species_id, species_name, vet_name, vet_id)
+VALUES (1, 'Pokemon', 'William Tatcher', 1),
+(1, 'Pokemon', 'Stephanie Mendez', 3),
+(2, 'Digimon', 'Stephanie Mendez', 3),
+(2, 'Digimon', 'Jack Harkness', 4);
+
+INSERT INTO visits (animal_id,animal_name,vet_id,vet_name,date_of_visit)
+VALUES (1,'Agumon', 1, 'William Tatcher', 'May 24th, 2020'),
+(1, 'Agumon', 3, 'Stephanie Mendez', 'Jul 22th, 2020'),
+(2, 'Gabumon', 4, 'Jack Harkness', 'Feb 2nd, 2021'),
+(3, 'Pikachu', 2, 'Maisy Smith', 'Jan 5th, 2020'),
+(3, 'Pikachu', 2, 'Maisy Smith', 'Mar 8th, 2020'),
+(3, 'Pikachu', 2, 'Maisy Smith', 'May 14th, 2020'),
+(3, 'Devimon', 3, 'Stephanie Mendez', 'May 4th, 2021'),
+(5, 'Charmander', 4, 'Jack Harkness', 'Feb 24th, 2021'),
+(6, 'Plantmon', 2, 'Maisy Smith', 'Dec 21st, 2019'),
+(6, 'Plantmon', 1, 'William Tatcher', 'Aug 10th, 2020'),
+(6, 'Plantmon', 2, 'Maisy Smith', 'Apr 7th, 2021'),
+(7, 'Squirtle', 3, 'Stephanie Mendez', 'Sep 29th, 2019'),
+(8, 'Angemon', 4, 'Jack Harkness', 'Oct 3rd, 2020'),
+(8, 'Angemon', 4, 'Jack Harkness', 'Nov 4th, 2020'),
+(9, 'Boarmon', 2, 'Maisy Smith', 'Jan 24th, 2019'),
+(9, 'Boarmon', 2, 'Maisy Smith', 'May 15th, 2019'),
+(9, 'Boarmon', 2, 'Maisy Smith', 'Feb 27th, 2020'),
+(9, 'Boarmon', 2, 'Maisy Smith', 'Aug 3rd, 2020'),
+(10, 'Blossom', 3, 'Stephanie Mendez', 'May 24th, 2020'),
+(10, 'Blossom', 1, 'William Tatcher', 'Jan 11th, 2021');
